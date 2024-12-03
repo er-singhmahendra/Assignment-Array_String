ccbd5a109ae8b997fcd06818440584089a633452
mahendra pratap singh <er.singhmahendra@gmail.com>
2024-12-03 23:25:39 +0530

    add

---
+++ array.js
@@ -0,0 +1,17 @@
+//task 1 - create array that store temperature recorded over 7 days
+
+var temperature = [20, 21, 19, 25, 23, 26, 27];
+
+//Task 2 - access temp of 4th day
+
+console.log (temperature[3]);        //result - 25
+
+//Task 3 - upadte it to a new value by adding 2 degrees
+
+temperature [3]= temperature[3]+2;
+
+//Task 4 - log the updated temperatures for all 7 days to the console
+
+console.log(temperature) 
+
+//done
